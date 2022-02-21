19772262c331d07567bc4fad5f6084a9c1420c65
xwwwb <xuwenbo@88.com>
2022-02-22 01:23:51 +0800

    一次性获取18周数据 最少程度漏课 肝不动了 睡觉睡觉

--- Parser.js
+++ Parser.js
@@ -1,130 +1,174 @@
+getDay = (day) => {
+	if (day === "一") {
+		return 1
+	} else if (day === "二") {
+		return 2
+	} else if (day === "三") {
+		return 3
+	} else if (day === "四") {
+		return 4
+	} else if (day === "五") {
+		return 5
+	} else if (day === "六") {
+		return 6
+	} else {
+		return 7
+	}
+}
+getSections = (sections) => {
+	let arr = sections.split("-")
+	let num_arr = []
+	arr.map((item) => {
+		num_arr.push(parseInt(item))
+	})
+	let new_arr = []
+	for (let i = num_arr[0]; i <= num_arr[1]; i++) {
+		new_arr.push(i)
+	}
+	return new_arr
+}
+getName = (name) => {
+	if (name == null) {
+		return "未知"
+	} else {
+		return name
+	}
+}
+getWeeks = (weeks) => {
+	let arr = weeks.split("-")
+	let num_arr = []
+	arr.map((item) => {
+		num_arr.push(parseInt(item))
+	})
+	let new_arr = []
+	for (let i = num_arr[0]; i <= num_arr[1]; i++) {
+		new_arr.push(i)
+	}
+	return new_arr
+}
+handleSingleWeeks = (weeks) => {
+	let arr = weeks.split("-")
+	let num_arr = []
+	arr.map((item) => {
+		num_arr.push(parseInt(item))
+	})
+	let new_arr = []
+	for (let i = num_arr[0]; i <= num_arr[1]; i++) {
+		if (i % 2 == 1) {
+			new_arr.push(i)
+		}
+	}
+	return new_arr
+}
+handleDoubleWeeks = (weeks) => {
+	let arr = weeks.split("-")
+	let num_arr = []
+	arr.map((item) => {
+		num_arr.push(parseInt(item))
+	})
+	let new_arr = []
+	for (let i = num_arr[0]; i <= num_arr[1]; i++) {
+		if (i % 2 == 0) {
+			new_arr.push(i)
+		}
+	}
+	return new_arr
+}
+
+// 以下三个开发暂未完成
+getWeeksSpec = (weeks) => {
+	let arr = weeks.split("-")
+	let num_arr = []
+	arr.map((item) => {
+		num_arr.push(parseInt(item))
+	})
+	let new_arr = []
+	for (let i = num_arr[0]; i <= num_arr[1]; i++) {
+		new_arr.push(i)
+	}
+	return new_arr
+}
+handleSingleWeeksSpec = (weeks) => {
+	let arr = weeks.split("-")
+	let num_arr = []
+	arr.map((item) => {
+		num_arr.push(parseInt(item))
+	})
+	let new_arr = []
+	for (let i = num_arr[0]; i <= num_arr[1]; i++) {
+		if (i % 2 == 1) {
+			new_arr.push(i)
+		}
+	}
+	return new_arr
+}
+handleDoubleWeeksSpec = (weeks) => {
+	let arr = weeks.split("-")
+	let num_arr = []
+	arr.map((item) => {
+		num_arr.push(parseInt(item))
+	})
+	let new_arr = []
+	for (let i = num_arr[0]; i <= num_arr[1]; i++) {
+		if (i % 2 == 0) {
+			new_arr.push(i)
+		}
+	}
+	return new_arr
+}
+
+
 function scheduleHtmlParser(html) {
 	let result = [] //结果
 	let duplicate = [] //查重用
 	let data = JSON.parse(html)
-	getDay = (day) => {
-		if (day === "一") {
-			return 1
-		} else if (day === "二") {
-			return 2
-		} else if (day === "三") {
-			return 3
-		} else if (day === "四") {
-			return 4
-		} else if (day === "五") {
-			return 5
-		} else if (day === "六") {
-			return 6
+	data.map((data) => {
+		if (data.success) {
+			let obj = data.obj
+			obj.map((item) => {
+				let weeks
+				// 长度小于5 说明是正常的周
+				if (item.courseWeekly.length < 5) {
+					if (item.courseSingleDoubleWeek == "0") {
+						weeks = getWeeks(item.courseWeekly)
+						console.log("不存在单双周问题")
+					} else if (item.courseSingleDoubleWeek == "1") {
+						weeks = handleSingleWeeks(item.courseWeekly)
+						console.log("此为单周课")
+					} else if (item.courseSingleDoubleWeek == "2") {
+						weeks = handleDoubleWeeks(item.courseWeekly)
+						console.log("此为双周课")
+					}
+				} else {
+					// 长度大于5 特殊周
+					if (item.courseSingleDoubleWeek == "0") {
+						weeks = getWeeksSpec(item.courseWeekly)
+						console.log("不存在单双周问题")
+					} else if (item.courseSingleDoubleWeek == "1") {
+						weeks = handleSingleWeeksSpec(item.courseWeekly)
+						console.log("此为单周课")
+					} else if (item.courseSingleDoubleWeek == "2") {
+						weeks = handleDoubleWeeksSpec(item.courseWeekly)
+						console.log("此为双周课")
+					}
+				}
+				let course = {
+					name: getName(item.courseName), // 课程名称
+					position: getName(item.courseClassRoomName), // 上课地点
+					teacher: getName(item.teacherName), // 教师名称
+					weeks, // 周数
+					day: getDay(item.courseDate), // 星期
+					sections: getSections(item.courseSection), // 节次
+				}
+				if (!duplicate.includes(item)) {
+					result.push(course)
+					duplicate.push(item)
+				}
+			})
 		} else {
-			return 7
+			console.log("导入失败")
 		}
-	}
-	getWeeks = (weeks) => {
-		let arr = weeks.split("-")
-		let num_arr = []
-		arr.map((item) => {
-			num_arr.push(parseInt(item))
-		})
-		let new_arr = []
-		for (let i = num_arr[0]; i <= num_arr[1]; i++) {
-			new_arr.push(i)
-		}
-		return new_arr
-	}
-	handleSingleWeeks = (weeks) => {
-		let arr = weeks.split("-")
-		let num_arr = []
-		arr.map((item) => {
-			num_arr.push(parseInt(item))
-		})
-		let new_arr = []
-		for (let i = num_arr[0]; i <= num_arr[1]; i++) {
-			if (i % 2 == 1) {
-				new_arr.push(i)
-			}
-		}
-		return new_arr
-	}
-
-	handleDoubleWeeks = (weeks) => {
-		let arr = weeks.split("-")
-		let num_arr = []
-		arr.map((item) => {
-			num_arr.push(parseInt(item))
-		})
-		let new_arr = []
-		for (let i = num_arr[0]; i <= num_arr[1]; i++) {
-			if (i % 2 == 0) {
-				new_arr.push(i)
-			}
-		}
-		return new_arr
-	}
-
-	getName = (name) => {
-		if (name == null) {
-			return "未知"
-		} else {
-			return name
-		}
-	}
-
-	if (data[0].success) {
-		let obj = data[0].obj
-		obj.map((item) => {
-			if (item.courseSingleDoubleWeek == "0") {
-				weeks = getWeeks(item.courseWeekly)
-				console.log("不存在单双周问题")
-			} else if (item.courseSingleDoubleWeek == "1") {
-				weeks = handleSingleWeeks(item.courseWeekly)
-				console.log("此为单周课")
-			} else if (item.courseSingleDoubleWeek == "2") {
-				weeks = handleDoubleWeeks(item.courseWeekly)
-				console.log("此为双周课")
-			}
-			let course = {
-				name: getName(item.courseName), // 课程名称
-				position: getName(item.courseClassRoomName), // 上课地点
-				teacher: getName(item.teacherName), // 教师名称
-				weeks, // 周数
-				day: getDay(item.courseDate), // 星期
-				sections: getWeeks(item.courseSection), // 节次
-			}
-			result.push(course)
-			duplicate.push(item) //录入的课程进入名单
-		})
-	} else {
-		console.log("还没开学")
-	}
-	if (data[1].success) {
-		obj = data[1].obj
-		obj.map((item) => {
-			if (item.courseSingleDoubleWeek == "1") {
-				weeks = handleSingleWeeks(item.courseWeekly)
-				console.log("此为单周课")
-			} else if (item.courseSingleDoubleWeek == "2") {
-				weeks = handleDoubleWeeks(item.courseWeekly)
-				console.log("此为双周课")
-			} else if (item.courseSingleDoubleWeek == "0") {
-				weeks = getWeeks(item.courseWeekly)
-				console.log("不存在单双周问题")
-			}
-			let course = {
-				name: getName(item.courseName), // 课程名称
-				position: getName(item.courseClassRoomName), // 上课地点
-				teacher: getName(item.teacherName), // 教师名称
-				weeks, // 周数
-				day: getDay(item.courseDate), // 星期
-				sections: getWeeks(item.courseSection), // 节次
-			}
-			if (!duplicate.includes(item)) {
-				result.push(course)
-				duplicate.push(item) //录入的课程进入名单
-			}
-		})
-	} else {
-		console.log("临近放假")
-	}
+	})
 
 	return result
 }
--- Provider.js
+++ Provider.js
@@ -1,8 +1,38 @@
+let handleTime = (time) => {
+	if (time < 10) {
+		return "0" + time
+	} else {
+		return time
+	}
+}
+// 生成18个日期 获取整个学期课表 防止遗漏
+let handleDate = () => {
+	let date_now = new Date()
+	let nowTime = date_now.getTime()
+	let oneWeek = 24 * 60 * 60 * 1000 * 7
+	let formatted_date = []
+	for (let i = 0; i < 18; i++) {
+		let date = new Date(nowTime + oneWeek * i)
+		let dateStr =
+			date.getFullYear() +
+			"-" +
+			handleTime(date.getMonth() + 1) +
+			"-" +
+			handleTime(date.getDate())
+
+		formatted_date.push(dateStr)
+	}
+
+	return formatted_date
+}
 async function scheduleHtmlProvider() {
 	await loadTool("AIScheduleTools")
-	await AIScheduleAlert("请务必打开'课程信息'页后 再执行导入操作 若还未开学或者临近期末 可能无法正确处理单双周课程")
+	await AIScheduleAlert(
+		"请务必打开'课程信息'页后 再执行导入操作 若还未开学或者临近期末 可能无法正确处理单双周课程 导入过程时间较长 请耐心等待"
+	)
 	let userid
 	let str
+	// 获取到学号
 	try {
 		const res = await fetch(
 			"http://iapp.zzuli.edu.cn/portal/app/casMember/getMemberByUsername",
@@ -18,112 +48,45 @@ async function scheduleHtmlProvider() {
 			})
 	} catch (error) {
 		await AIScheduleAlert("请务必打开'课程信息'页后 再执行导入操作 ")
-		return 'do not continue'
+		return "do not continue"
 	}
 
-	let handleTime = (time) => {
-		if (time < 10) {
-			return "0" + time
-		} else {
-			return time
-		}
+	let formatted_date = handleDate()
+
+	let body = []
+	for (let i = 0; i < 18; i++) {
+		let _body =
+			"userName=" +
+			parseInt(userid) +
+			"&currentTime=" +
+			formatted_date[i] +
+			"&role=1"
+
+		body.push(_body)
 	}
-	let handleDate = () => {
-		let date_1 = new Date()
-		let nowTime = date_1.getTime()
-		let oneWeek = 24 * 60 * 60 * 1000 * 7
-		let date_2 = new Date(nowTime + oneWeek)
-		let formatted_date = []
-
-		let dateStr_1 =
-			date_1.getFullYear() +
-			"-" +
-			handleTime(date_1.getMonth() + 1) +
-			"-" +
-			handleTime(date_1.getDate())
-
-		let dateStr_2 =
-			date_2.getFullYear() +
-			"-" +
-			handleTime(date_2.getMonth() + 1) +
-			"-" +
-			handleTime(date_2.getDate())
-
-		formatted_date = [dateStr_1, dateStr_2]
-		return formatted_date
-	}
-
-	console.log(handleDate())
 
 	let result = []
-	body =
-		"userName=" +
-		parseInt(userid) +
-		"&currentTime=" +
-		handleDate()[0] +
-		"&role=1"
-
-	console.log(body)
 	url = "http://microapp.zzuli.edu.cn/microapplication/app/course/getCourse"
-	try {
-		const res = await fetch(url, {
-			method: "POST",
-			body: body,
-			headers: {
-				"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-			},
-			credentials: "include",
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				// if (!data.success) {
-				// 	AIScheduleAlert(
-				// 		"还没开学？无法获取这周课表。可能无法正确导入单双周课程。"
-				// 	)
-				// }
-				// str = JSON.stringify(data)
 
-				result.push(data)
+	for (let i = 0; i < 18; i++) {
+		try {
+			const res = await fetch(url, {
+				method: "POST",
+				body:body[i],
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+				},
+				credentials: "include",
 			})
-	} catch (error) {
-		console.error(error)
-		await AIScheduleAlert(error.message)
-		return "do not continue"
-	}
-
-	body =
-		"userName=" +
-		parseInt(userid) +
-		"&currentTime=" +
-		handleDate()[1] +
-		"&role=1"
-
-	console.log(body)
-	url = "http://microapp.zzuli.edu.cn/microapplication/app/course/getCourse"
-	try {
-		const res = await fetch(url, {
-			method: "POST",
-			body: body,
-			headers: {
-				"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-			},
-			credentials: "include",
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				// if (!data.success) {
-				// 	AIScheduleAlert(
-				// 		"快期末了？无法获取下周课表。可能无法正确导入单双周课程。"
-				// 	)
-				// }
-				// str = JSON.stringify(data)
-
-				result.push(data)
-			})
-	} catch (error) {
-		console.error(error)
-		await AIScheduleAlert(error.message)
-		return "do not continue"
+				.then((response) => response.json())
+				.then((data) => {
+					// console.log(data)
+					result.push(data)
+				})
+		} catch (error) {
+			console.log(error)
+			result.push({ success: false })
+		}
 	}
 
 	return JSON.stringify(result)
